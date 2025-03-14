c138a7d585d94045e49929399ead5ee9f875049e
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-03-14 17:43:28 +0200

    Added Ex2-IsLeapYear solution

---
+++ level-3/Ex2-IsLeapYear/Ex2-IsLeapYear.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "..\..\..\MyLibrary\MyInputLib.h"
+
+using namespace std;
+
+bool IsLeapYear(short Year) {
+    return Year % 400 == 0 ? true : (Year % 4 == 0 && Year % 100 != 0);
+}
+
+int main()
+{
+    short Year = MyInputLib::ReadPositiveNumber("Enter a year : ");
+    
+    if (IsLeapYear(Year))
+        printf("\n%d is leap year", Year);
+    else
+        printf("\n%d is not leap year", Year);
+
+    return 0;
+}
